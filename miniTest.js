1841368dbdc907911218ee277df589bcf7bcb838
baoht42 <snorlax4293@gmail.com>
2023-11-11 17:41:49 +0700

    nop bai 1,2

---
+++ miniTest.js
@@ -0,0 +1,18 @@
+//bai tap 1
+function bai1() {
+    let inputNum = +prompt('Nhap so: ')
+    let outputNum = inputNum * inputNum;
+    alert('Binh phuong cua so nhap vao la: ' + outputNum);
+}
+//bai tap 2
+function bai2() {
+    let num1 = +document.getElementById('text1').value;
+    let num2 = +document.getElementById('text2').value;
+    let S = num1 * num2;
+    document.getElementById('result').innerText = S;
+}
+
+//bai tap 3
+function bai3(){
+    let
+}
