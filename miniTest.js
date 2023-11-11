9be5d4255ca9cb79382b57d84032a77220adac04
baoht42 <snorlax4293@gmail.com>
2023-11-11 18:19:09 +0700

    nop bai 3a,3b

--- miniTest.js
+++ miniTest.js
@@ -14,5 +14,15 @@ function bai2() {
 
 //bai tap 3
 function bai3(){
-    let
+    let a = +document.getElementById('numA').value;
+    let b = +document.getElementById('numB').value;
+    let c = +document.getElementById('numC').value;
+let bai3_a1 = a * 4;
+let bai3_a2 = a * a;
+document.getElementById('bai3_a').innerHTML= 'Chu vi: ' + bai3_a1 +'  ' + 'Dien tich: ' + bai3_a2;
+let bai3_b1 = a**a;
+let bai3_b2 = a**b;
+document.getElementById('bai3_b').innerHTML= 'a^a = '+bai3_b1 + '  ' + 'a^b: ' +bai3_b2;
+
+
 }
